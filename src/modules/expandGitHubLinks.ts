f3115206915e51fdb6fac608ca5709feea743014
uncenter <47499684+uncenter@users.noreply.github.com>
2023-10-24 10:08:30 -0400

    fix: limit github code block embed lengths

--- src/modules/expandGitHubLinks.ts
+++ src/modules/expandGitHubLinks.ts
@@ -3,6 +3,9 @@ import type { Message } from 'discord.js';
 
 import { EmbedBuilder, Events } from 'discord.js';
 
+import { HEX_PURPLE } from '~/utils/constants';
+import { logger } from '~/utils/logger';
+
 const regex =
 	/https?:\/\/github\.com\/([\w-]+\/[\w.-]+)\/blob\/(.+?)\/(.+?)#L(\d+)[~-]?L?(\d*)/g;
 
@@ -11,8 +14,12 @@ export async function expandGitHubLinksModule(bot: Bot) {
 		const { content } = message;
 		if (!regex.test(content)) return;
 
-		let codeBlocks: { language: string; content: string; name: string }[] =
-			[];
+		let codeBlocks: {
+			language: string;
+			content: string;
+			name: string;
+			body?: string;
+		}[] = [];
 
 		content.match(regex);
 
@@ -20,27 +27,52 @@ export async function expandGitHubLinksModule(bot: Bot) {
 			const [fullURL, repo, ref, file, startStr, endStr] = match;
 
 			const start = Number.parseInt(startStr);
-			const end = endStr ? Number.parseInt(endStr) : null;
-			const name = `${repo}@${
-				ref.length === 40 ? ref.slice(0, 8) : ref
-			} ${file} L${start}${end ? `-${end}` : ''}`;
+			let end = endStr ? Number.parseInt(endStr) : null;
 			const language = new URL(fullURL).pathname.split('.').at(-1) || '';
 
 			const text = await fetch(
 				`https://raw.githubusercontent.com/${repo}/${ref}/${file}`,
 			).then((res) => {
 				if (!res.ok)
-					throw new Error(`Failed to fetch ${fullURL} contents`);
+					logger.error(`Failed to fetch ${fullURL} contents.`);
 				return res.text();
 			});
 			if (Number.isNaN(start) || Number.isNaN(end)) continue;
 
-			const content = text
+			let content = text
 				.split('\n')
 				.slice(start - 1, end === null ? start : end)
 				.join('\n');
 
-			codeBlocks.push({ name, language, content });
+			// Ensure code blocks won't go over message length limit (2000 characters).
+			let linesSliced = 0;
+			// eslint-disable-next-line no-constant-condition
+			while (true) {
+				if (content.length < 1970) break;
+
+				const lines = content.split('\n');
+				if (lines.length === 1) {
+					content = content.slice(0, 1970);
+					break;
+				}
+				lines.pop();
+				content = lines.join('\n');
+				linesSliced += 1;
+			}
+			(end as number) -= linesSliced;
+
+			// Ex. "user123/some-repo@main package.json L1-11".
+			const name = `${repo}@${
+				ref.length === 40 ? ref.slice(0, 8) : ref
+			} ${file} L${start}${end ? `-${end}` : ''}`;
+
+			// Ex. "... (1966 lines not displayed)".
+			const body =
+				linesSliced > 0
+					? `... (${linesSliced} lines not displayed)`
+					: '';
+
+			codeBlocks.push({ name, language, content, body });
 		}
 
 		codeBlocks = codeBlocks.filter((block) => !!block.content.trim());
@@ -51,10 +83,10 @@ export async function expandGitHubLinksModule(bot: Bot) {
 				embeds: codeBlocks.map((block) =>
 					new EmbedBuilder()
 						.setDescription(
-							`\`\`\`${block.language}\n${block.content}\n\`\`\``,
+							`\`\`\`${block.language}\n${block.content}\n\`\`\`\n${block.body}`,
 						)
 						.setAuthor({ name: block.name })
-						.setColor(0xa7f3d0),
+						.setColor(HEX_PURPLE),
 				),
 				allowedMentions: { repliedUser: false, roles: [], users: [] },
 			});
